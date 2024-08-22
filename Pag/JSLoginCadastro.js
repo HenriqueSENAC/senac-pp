076cd480bb904fa11651c630023ab50b13bdb7e2
HENRIQUE ATTOLINI TERRA <05762676005@senacrs.edu.br>
2024-08-22 12:39:55 -0300

    commit

--- Pag/JSLoginCadastro.js
+++ Pag/JSLoginCadastro.js
@@ -55,13 +55,9 @@ button.onclick = async function() {
         let content = await response.json();    
 
         if(content.success) {
-          alert("Login feito") 
-        //   login feito ta indo pra tudo
-        //   Quando login não funciona é uma trabalheira pra identificar um motivo
-        //   então eu só coloquei que as credenciais não batem, pode não ser a maneira
-        //   mais refinada mas funciona do jeito que é pra funcionar
-        //   Criar um código pra quando os valores enviados não baterem com o data
-        //   ele não envie mensagem de sucesso
+          alert("Login feito");
+          window.location.replace("MainPage.html")
+
         } else {
           alert('Error')
         }
@@ -87,4 +83,4 @@ function LoginFunction() {
     } else {
         x.type = "password";
     }
-}
+}   
