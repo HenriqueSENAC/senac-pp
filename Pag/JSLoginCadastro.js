29356e7340909bf537b5676aa3acca70586e1427
Kridtin16 <henriqueroblox@gmail.com>
2024-08-23 19:15:16 -0300

    Telas arrumadas porem o js parou do nada

--- Pag/JSLoginCadastro.js
+++ Pag/JSLoginCadastro.js
@@ -15,11 +15,12 @@ async function cadastrar(event) {
             // Simplesmente não quer funcionar!!! 
         } else {
             let data = {email, senha, cel}
-
+            window.location.replace("Login.html");
             const response = await fetch('http://localhost:3005/api/store/user', {
                 method: "POST",
                 headers: {"Content-type": "application/json;charset=UTF-8"},
                 body: JSON.stringify(data)
+
             });
         }
     }
@@ -49,7 +50,7 @@ button.onclick = async function() {
 
         if(content.success) {
           alert("Login feito");
-          window.location.replace("MainPage.html")
+        //   window.location.replace("MainPage.html")
 
         } else {
           PSWarningLogin();
