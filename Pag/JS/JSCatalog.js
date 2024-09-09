a1e4873b7655cbd758a975e9508f9fb382704f8d
HENRIQUE ATTOLINI TERRA <05762676005@senacrs.edu.br>
2024-09-09 12:18:58 -0300

    catalog

---
+++ Pag/JS/JSCatalog.js
@@ -0,0 +1,21 @@
+let button = document.getElementById('filename');
+
+button.onclick = async function(e) {
+    e.preventDefault();
+
+    
+}
+
+
+function Checkfiles(){
+    var fup = document.getElementById('filename');
+    var fileName = fup.value;
+    var ext = fileName.substring(fileName.lastIndexOf('.') + 1);
+
+    if(ext =="jpeg" || ext=="png"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
